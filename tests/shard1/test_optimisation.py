7a2fb4410aef55b8274223c9c18f9780f2315dc1
Math <math.math@caramail.fr>
2025-11-18 21:22:53 -0500

    fix(test_optimisation)

--- tests/shard1/test_optimisation.py
+++ tests/shard1/test_optimisation.py
@@ -17,21 +17,21 @@ def test_optimisation_svc_model():
     X, y, _ = Process.prepare_data(data_path, features_path, "VIT", model_name=model.name)
     model.add_data(X, y, test_size=0.2)
 
-    # model.train_and_tune("bayesian_optim", n_iter=50)
-    # y_pred = model.model.predict(model.X_test_scaled)
+    model.train_and_tune("bayesian_optim", n_iter=20)
+    y_pred = model.model.predict(model.X_test_scaled)
 
-    # np.testing.assert_equal(
-    #     np.array([model.model.C, float(model.model.gamma), model.model.degree]),
-    #     np.array([169.63021952277953, 0.10992366904399258, 3]),
-    # )
+    np.testing.assert_equal(
+        np.array([model.model.C, float(model.model.gamma), model.model.degree]),
+        np.array([375.16557872851513, 0.9507635921035062, 4]),
+    )
 
-    # np.testing.assert_equal(np.array(y_pred), np.array([1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1]))
+    np.testing.assert_equal(np.array(y_pred), np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
 
-    model.train_and_tune("bayesian_search", n_iter=10)
+    model.train_and_tune("bayesian_search", n_iter=20)
     np.testing.assert_equal(
         np.array([model.model.C, model.model.gamma, model.model.degree]),
-        np.array([410.6938548944605, 0.09335393188593556, 4]),
+        np.array([445.3876797888507, 0.011381056696717656, 5]),
     )
     y_pred = model.model.predict(model.X_test_scaled)
 
-    np.testing.assert_equal(np.array(y_pred), np.array([0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0]))
+    np.testing.assert_equal(np.array(y_pred), np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
