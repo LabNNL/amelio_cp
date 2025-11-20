328500f5ad2c3940c3ccbb99596b847acc5d94cf
Math <math.math@caramail.fr>
2025-11-19 23:37:21 -0500

    prediction_analysis: for loop to change the random_sate_split

--- examples/prediction_analysis/prediction_analysis_svc.py
+++ examples/prediction_analysis/prediction_analysis_svc.py
@@ -7,6 +7,10 @@ import numpy as np
 import pandas as pd
 import pickle as pkl
 
+def build_model(rdm_state):
+    model = SVCModel()
+    model.random_state_split = rdm_state
+    return model
 
 def prepare_data(data_path, features_path, condition_to_predict):
     all_data = Process.load_csv(data_path)
@@ -48,7 +52,7 @@ def collect_wrong_predictions(y_1, y_2, idx):
     return FP, FN
 
 
-def plot_distributions(data, wrong_preds, features, condition_to_predict, output_path):
+def plot_distributions(data, wrong_preds, features, condition_to_predict, random_state, output_path):
     for i in range(len(wrong_preds)):
         fig, axes = plt.subplots(5, 4, figsize=(50, 50))
         m, n = 0, 0
@@ -58,16 +62,16 @@ def plot_distributions(data, wrong_preds, features, condition_to_predict, output
             if n == 4:
                 n = 0
                 m += 1
-        plt.suptitle(f"Distributions for patient index: {wrong_preds[i]}")
+        plt.suptitle(f"Distributions for patient index: {wrong_preds[i]}\n(random_state={random_state})", fontsize=40)
         plt.savefig(
-            f"{output_path}distribution_for_{wrong_preds[i]}_{condition_to_predict}.svg", dpi=300, bbox_inches="tight"
+            f"{output_path}distribution_for_{wrong_preds[i]}_{condition_to_predict}_{random_state}.svg", dpi=300, bbox_inches="tight"
         )
 
 
 def append_data(results_dict, condition_to_predict, model, accuracy, y_true, y_pred, FP, FN):
-    results_dict[condition_to_predict] = {
+    results_dict[model.random_state] = {
+        "condition_to_predict": condition_to_predict,
         "optim_method": model.optim_method,
-        "random_state": model.random_state,
         "C": model.best_params["C"],
         "gamma": model.best_params["gamma"],
         "degree": model.best_params["degree"],
@@ -83,41 +87,44 @@ def append_data(results_dict, condition_to_predict, model, accuracy, y_true, y_p
 
 
 def save_data(results_dict, condition_to_predict, output_path):
-    pickle_file_name = output_path + "_" + condition_to_predict + "_gains.pkl"
+    pickle_file_name = output_path + "_" + condition_to_predict + ".pkl"
     with open(pickle_file_name, "wb") as file:
         pkl.dump(results_dict, file)
 
 
-def main(condition_to_predict):
+def main(condition_to_predict, list_random_state):
     results_dict = {}
     data_path = "datasets/sample_2/all_data_28pp.csv"
     features_path = "amelio_cp/processing/Features.xlsx"
     output_path = "examples/results/prediction_analysis_results/"
 
-    X, y, features = prepare_data(data_path, features_path, condition_to_predict)
-    model = SVCModel()
-    load_data(X, y, model)
+    for random_state in list_random_state:
+        X, y, features = prepare_data(data_path, features_path, condition_to_predict)
+        model = build_model(random_state)
+        load_data(X, y, model)
 
-    model.train_and_tune("bayesian_optim")
-    y_pred = model.model.predict(model.X_test_scaled)
-    accuracy = model.model.score(model.X_test_scaled, model.y_test)
+        model.train_and_tune("bayesian_optim")
+        y_pred = model.model.predict(model.X_test_scaled)
+        accuracy = model.model.score(model.X_test_scaled, model.y_test)
 
-    ClassifierMetrics.conf_matrix(
-        model,
-        model.y_test,
-        y_pred,
-        ["Responders", "Non-Responders"],
-        "Confusion_Matrix",
-    )
+        ClassifierMetrics.conf_matrix(
+            model,
+            model.y_test,
+            y_pred,
+            ["Responders", "Non-Responders"],
+            "Confusion_Matrix",
+        )
 
-    FP, FN = collect_wrong_predictions(np.array(model.y_test), y_pred, model.y_test.index)
-    plot_distributions(X, FP, features, condition_to_predict, output_path + "false_positives_")
-    plot_distributions(X, FN, features, condition_to_predict, output_path + "false_negatives_")
+        FP, FN = collect_wrong_predictions(np.array(model.y_test), y_pred, model.y_test.index)
+        plot_distributions(X, FP, features, condition_to_predict, random_state, output_path + "false_positives_")
+        plot_distributions(X, FN, features, condition_to_predict, random_state, output_path + "false_negatives_")
 
-    append_data(results_dict, condition_to_predict, model, accuracy, model.y_test, y_pred, FP, FN)
+        append_data(results_dict, condition_to_predict, model, accuracy, model.y_test, y_pred, FP, FN)
+    
     save_data(results_dict, condition_to_predict, output_path)
 
 
 if __name__ == "__main__":
-    main("VIT")
-    main("6MWT")
+    list_random_state = [42, 72, 36, 8, 17]
+    main("VIT", list_random_state)
+    # main("6MWT")
