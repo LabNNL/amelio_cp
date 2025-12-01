37c406a91e003f69e3e91335bf64c16385fad8aa
Math <math.math@caramail.fr>
2025-12-01 09:26:21 -0500

    tests: fake data and tests rearranged + few new tests
    random_state, 3 methods for svc

--- tests/shard1/test_models.py
+++ tests/shard1/test_models.py
@@ -52,6 +52,29 @@ def test_classifier_model():
         np.array([42, 1, 1000, 0.001, 0.1, 2, 5, 3]),
     )
 
+    np.testing.assert_almost_equal(
+        np.array(
+            [
+                model.random_state,
+                model.random_state_split,
+                model.random_state_cv,
+                model.random_state_optim,
+            ]),
+            np.array([42, 42, 42, 42]),
+    )
+
+    model.random_state = 20
+
+    np.testing.assert_almost_equal(
+        np.array(
+            [
+                model.random_state,
+                model.random_state_split,
+                model.random_state_cv,
+                model.random_state_optim,
+            ]),
+            np.array([20, 42, 42, 42]),
+    )
 
 def test_svr_model():
     model = SVRModel()
@@ -158,13 +181,22 @@ def test_svr_model():
     np.testing.assert_almost_equal(
         model.X_train_scaled[0],
         np.array(
-            [ 0.55228733,  0.99717765, -0.55407153, -0.37225737,  1.17974668,
-        0.45605703,  0.191773  ,  0.90265365,  0.12127846,  1.52220883,
-        0.60995466, -0.63322088, -0.00859222,  1.16535424,  0.04049381,
-        0.54281566, -0.28125767,  0.78284974,  0.64828423]
+            [-0.81394551, -0.9235252 ,  0.20456801, -0.08627907, -0.10311143,
+        0.39241331, -0.1839471 ,  1.30447769,  1.52120961,  0.30404797,
+       -0.13384099,  0.83412958, -0.12237037, -1.13388565, -0.31966485,
+       -0.27028201, -0.11063276, -1.06845727,  0.66369256]
         ),
     )
 
+    np.testing.assert_almost_equal(
+        model.X_test_scaled[10],
+        np.array(
+            [-0.57346513, -0.11378246,  0.22711145,  0.1438525 , -0.87268322,
+       -0.32667434,  2.23228398, -0.16715238, -0.29976348,  1.86015765,
+        1.08191249, -0.20763502,  0.14090204, -1.23881797,  0.53998826,
+        0.56578258,  1.26804485,  1.91693827, -0.02433134]
+        ),
+    )
     # TODO: X_test_scaled[i]
 
 
@@ -179,5 +211,42 @@ def test_svc_model():
         np.array([50, 0.01, 4]),
     )
 
+    X, y, _ = Process.prepare_data(data_path, "VIT", model_name=model.name, features_path=None)
+    model.add_data(X, y, test_size=0.2)
 
-# TODO: model.add_train_data and add_test_data tests w/ fake data and compare
+    np.testing.assert_equal(np.array(model.X_train.shape), np.array((80, 19)))
+    np.testing.assert_equal(
+        np.array(model.X_train.index),
+        np.array([ 29,  73,  59,  90,  20,   0,  97,  46,  81,  28,  12,  33,  53,  65,
+        77,  34,  85,  69,  87,   3,  70,  56,  47,  42,  19,  22,  82,  50,
+        13,  71,  14,  27,  72,  84,  54,  15,  26,  60,  58,  36,  68,  48,
+        30,  16,  10,  76,   8,  31,  57,  78,  83,  17,  79,  45,  37,  91,
+        41,  18,  39,  52,   4,  95,  38,  62, 100,   6,  24,   7,  44,  86,
+        89,  49,  66,  74,   5,  75,   9,  51,  11,  43],
+        )
+    )
+    
+    np.testing.assert_almost_equal(
+        model.X_train_scaled[0],
+        np.array(
+            [-0.13342089,  0.64625085,  0.93100641, -0.27789055,  0.84442693,
+       -0.89071163, -1.44651713, -0.91519077,  1.2878339 ,  1.38228583,
+       -0.10834961,  0.30550508,  0.55596304, -1.63477059, -0.62099416,
+       -1.11153157,  0.92986218,  0.56831813, -1.3680418 ]
+        ),
+    )
+
+    np.testing.assert_almost_equal(
+        model.y_test.index,
+        np.array(
+            [88, 67, 21, 25,  2, 55, 40, 96, 63, 93, 35, 61, 98, 64, 99,  1, 92, 80,
+       32, 94, 23]
+        )
+    )
+
+    np.testing.assert_almost_equal(
+        model.y_test.to_list(),
+        np.array(
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        )
+    )
--- tests/shard1/test_optimisation.py
+++ tests/shard1/test_optimisation.py
@@ -16,21 +16,33 @@ def test_optimisation_svc_model():
     X, y, _ = Process.prepare_data(data_path, "VIT", model_name=model.name)
     model.add_data(X, y, test_size=0.2)
 
-    model.train_and_tune("bayesian_optim", n_iter=20)
+    # Bayesian Optimization
+    model.train_and_tune("bayesian_optim", n_iter=30)
     y_pred = model.model.predict(model.X_test_scaled)
 
     np.testing.assert_equal(
         np.array([model.model.C, float(model.model.gamma), model.model.degree]),
-        np.array([375.16557872851513, 0.9507635921035062, 4]),
+        np.array([832.6101981596213, 0.21312677156759788, 2]),
     )
 
-    np.testing.assert_equal(np.array(y_pred), np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+    np.testing.assert_equal(np.array(y_pred), np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1]))
 
-    model.train_and_tune("bayesian_search", n_iter=20)
+    # Bayesian Search
+    model.train_and_tune("bayesian_search", n_iter=30)
     np.testing.assert_equal(
         np.array([model.model.C, model.model.gamma, model.model.degree]),
-        np.array([445.3876797888507, 0.011381056696717656, 5]),
+        np.array([837.5509671977311, 0.031037600878533328, 5]),
     )
     y_pred = model.model.predict(model.X_test_scaled)
 
-    np.testing.assert_equal(np.array(y_pred), np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+    np.testing.assert_equal(np.array(y_pred), np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1]))
+    
+    # Random Search
+    model.train_and_tune("random", n_iter=30)
+    np.testing.assert_equal(
+        np.array([model.model.C, model.model.gamma, model.model.degree]),
+        np.array([525.7564316322379, 0.030122914019804194, 2]),
+    )
+    y_pred = model.model.predict(model.X_test_scaled)
+
+    np.testing.assert_equal(np.array(y_pred), np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1]))
