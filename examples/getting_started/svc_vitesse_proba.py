ea3c41c60c4a6446797564faddfa6c9edbf3ee3a
Math <math.math@caramail.fr>
2025-12-16 11:29:03 -0500

    svc_vitesse_proba: added script to test model with proba & display it

--- examples/getting_started/svc_vitesse_proba.py
+++ examples/getting_started/svc_vitesse_proba.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from amelio_cp import Process
-from amelio_cp import SVCModel
+from amelio_cp import SVCModelProba
 from amelio_cp import ClassifierMetrics
 from sklearn.metrics import classification_report
 from amelio_cp import SHAPPlots
+from amelio_cp import LollipopPlot
 
 # %% Collecting/Loading the data from a csv file already created
 file_path = "datasets/sample_2/all_data_28pp.csv"
@@ -21,24 +22,23 @@ delta_vit = Process.calculate_MCID(all_data["VIT_PRE"], all_data["VIT_POST"], "V
 all_data_vit = all_data.drop(["6MWT_POST"], axis=1)
 all_data_vit = all_data_vit.dropna()
 
-
 data_vit = all_data_vit[selected_features]
 print("Number of participants for speed classification:", data_vit.shape[0])
 print(data_vit.columns)
 
 # %% Training the models
 
-SVC_vit = SVCModel()
-SVC_vit.random_state = 42
+SVC_vit = SVCModelProba()
 SVC_vit.add_data(data_vit, delta_vit, 0.2)
 SVC_vit.train_and_tune("bayesian_optim")
 print("Best parameters found for speed classification model: \n", SVC_vit.best_params)
 
+SVC_vit.probabilities = SVC_vit.model.predict_proba(SVC_vit.X_test_scaled)
 y_pred_vit_classif = SVC_vit.model.predict(SVC_vit.X_test_scaled)
 print("Accuracy test score: ", SVC_vit.model.score(SVC_vit.X_test_scaled, SVC_vit.y_test))
 print(classification_report(SVC_vit.y_test, y_pred_vit_classif), flush=True)
 
-output_path = "examples/results/svc_vs_svr_rdm_state/"
+output_path = "examples/results/svc_with_proba/"
 ClassifierMetrics.conf_matrix(
     SVC_vit,
     SVC_vit.y_test,
@@ -48,5 +48,19 @@ ClassifierMetrics.conf_matrix(
     output_path=output_path,
 )
 
+class0_proba = [-prob[0] for prob in SVC_vit.probabilities]
+class1_proba = [prob[1] for prob in SVC_vit.probabilities]
+y_positions = SVC_vit.y_test.index.tolist()
+class0_ordered, class1_ordered, y_ordered = LollipopPlot.order_values(class0_proba, class1_proba, y_positions)
+
+LollipopPlot().plot_lollipop(
+    class0_ordered,
+    class1_ordered,
+    y_ordered,
+    labels=["Class 0", "Class 1"],
+    title="Predicted Probabilities for Speed Classification",
+    output_path=output_path,
+)
+
 SVC_vit.shap_analysis = SHAPPlots.shap_values_calculation(SVC_vit)
-SHAPPlots.plot_shap_summary(SVC_vit, features_names, show=True)
+SHAPPlots.plot_shap_summary(SVC_vit, features_names, output_path=output_path, show=True)
